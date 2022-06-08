7e53ffa3ebbb0da3a08795090e5ebeb8017c4675
Unripe01 <tomato.ripe.dev@gmail.com>
2022-06-09 00:26:33 +0900

    add:server

---
+++ src/server/index.ts
@@ -0,0 +1,47 @@
+import cors from "cors";
+import express, { NextFunction } from "express";
+const app = express();
+
+//----------------------------------------------------
+//
+//deal with cors
+app.use(cors());
+
+//----------------------------------------------------
+//
+// Route
+app.get("/api/health", (req, res) => {
+  res.send({ message: "pong" });
+});
+
+//----------------------------------------------------
+//
+// Routeに一致しないRequest
+app.use((req, res, next) => {
+  res.sendStatus(404);
+  next({ statusCode: 404 }); // エラーロギング！
+});
+
+//----------------------------------------------------
+//
+// Error Route
+app.use(
+  (
+    err: { statusCode: number },
+    req: Express.Request,
+    res: Express.Response,
+    next: NextFunction // next() に渡されたコードを最終のエラーハンドラであるここのerrで受け取るために、第4引数を指定している
+  ) => {
+    console.log(err.statusCode);
+  }
+);
+
+//----------------------------------------------------
+//
+// Express Serverの起動
+const port = 8888;
+const host = "localhost";
+
+app.listen(port, host, () => {
+  console.log(`Running on http://${host}:${port}`);
+});
